390eb07103d8fba8949784dd644a06f5cc3f7e3a
Ubuntu <ubuntu@ip-10-164-86-29.ec2.internal>
2013-07-08 14:47:48 +0000

    updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 	var buffer = new Buffer(100);
-	buffer.write(fs.readFileSync('index.html'));
+	buffer.write(fs.readFileSync('index.html', 'utf-8'));
 	response.send(buffer.toString());
 });
 
