4105da284e957919471bbe7e24ff9eeab320f648
Ubuntu <ubuntu@ip-10-245-126-130.ec2.internal>
2013-07-10 03:22:08 +0000

    updated checks.json and grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,10 +66,31 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Entire url of your Heroku app')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    
+    if (program.url) {
+      restler.get(program.url).on('complete', function(result) {
+	if (result instanceof Error) {
+	}
+	else {
+		$ = cheerio.load(result);
+		var checks = loadChecks(program.checks).sort();
+    		var out = {};
+    		for(var ii in checks) {
+        		var present = $(checks[ii]).length > 0;
+        		out[checks[ii]] = present;
+    		}
+		var outJson = JSON.stringify(out, null, 4);
+		console.log(outJson);
+	}
+      });
+    }
+    else {
+    	var checkJson = checkHtmlFile(program.file, program.checks);
+    	var outJson = JSON.stringify(checkJson, null, 4);
+   	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
