e1960633429c75b49bf731a2decd87071040932f
Ubuntu <ubuntu@ip-10-236-160-55.ec2.internal>
2013-07-16 14:58:06 +0000

    homework 4 part 1

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-	var buffer = new Buffer(10000);
+	var buffer = new Buffer(15000);
 	var bufferLength = buffer.write(fs.readFileSync('index.html', 'utf-8'));
 	response.send(buffer.toString("utf-8", 0, bufferLength));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
